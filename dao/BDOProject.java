5c6fef30094513334452ca6c0a4d0defa0d294df
Ataburjee <atabur8119921@gmail.com>
2022-10-02 02:34:32 +0530

    day-2

---
+++ dao/BDOProject.java
@@ -0,0 +1,34 @@
+package com.masai.dao;
+
+import java.util.List;
+
+import com.masai.bean.GPM;
+import com.masai.bean.Project;
+import com.masai.bean.Worker;
+import com.masai.exceptions.EmployeeException;
+import com.masai.exceptions.EmployeeException;
+import com.masai.exceptions.GPMException;
+import com.masai.exceptions.ProjectException;
+
+public interface BDOProject {
+	
+	public boolean BDOLoginCred(String username, String password) throws ProjectException;
+	
+	public String allocateProjects(Project project);
+
+	public List<Project> getAllProject() throws ProjectException;
+	
+//	public String allocateGPM(int mid, String mname, String panchayet, String password) 
+//			throws ProjectException, GPMException;
+	
+	public String allocateGPM(GPM gpm) throws ProjectException, GPMException;
+	
+	public List<GPM> getAllGPM() throws GPMException;
+	
+	public boolean GPMLoginCred(int userId, String password) throws ProjectException, GPMException;
+	
+	public String assignEmployeeToProject(Worker worker);
+	
+	public List<Worker> getAllEmployee() throws EmployeeException;
+	
+}
