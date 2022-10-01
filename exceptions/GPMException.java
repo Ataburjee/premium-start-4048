e5cf0a3ea289cfe61f3213ea18e6caac4f571c57
Ataburjee <atabur8119921@gmail.com>
2022-10-02 03:05:44 +0530

    day-4

---
+++ exceptions/GPMException.java
@@ -0,0 +1,14 @@
+package com.masai.exceptions;
+
+public class GPMException extends Exception{
+
+	public GPMException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public GPMException(String messege) {
+		
+		super(messege);
+	}
+	
+}
